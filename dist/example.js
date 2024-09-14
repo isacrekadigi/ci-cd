eb78f5a349c7de2fd4490687858f8538a045a549
isacrekadigi <168390873+isacrekadigi@users.noreply.github.com>
2024-09-14 10:47:22 +0700

    initial project (#1)
    squash - project init

---
+++ dist/example.js
@@ -0,0 +1,8 @@
+"use strict";
+// example.js
+const unusedVar = 10;
+function greet(name) {
+    if (name == 'John') {
+        console.log("Hello, John");
+    }
+}
---
+++ src/server.ts
@@ -0,0 +1,5 @@
+// src/index.ts
+export const greet = (name: string): string => {
+  const idad = "user";
+  return `Hello, ${name}!`;
+};

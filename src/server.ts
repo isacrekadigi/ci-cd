6655733578cb4568056250a0350e7f7d9915b657
isacrekadigi <rinandaisac@rekadigi.id>
2024-09-18 10:04:51 +0700

    test ci/cd

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 // src/index.ts
 export const greet = (name: string): string => {
-                const idad = 'user';
-  return  `Hello, ${name}!`;
+  // const idad = "user";
+  return `Hello, ${name}!`;
 };
